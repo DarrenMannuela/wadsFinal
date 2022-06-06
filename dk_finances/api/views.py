b743acca067fc61095a5c5bc1c1f2cdd89fcf4ea
DarrenMannuela <darren.mannuela@gmail.com>
2022-06-06 12:09:36 +0700

    added imports

--- dk_finances/api/views.py
+++ dk_finances/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Account, BudgetAllocation, History, Balance, Income
 from .serializers import (AccountSerializer, BudgetAllocationSerializer, CreateAccountSerializer,
