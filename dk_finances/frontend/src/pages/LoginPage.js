8491dc3abb57fa446d799f896e847b8eff6842f5
DarrenMannuela <darren.mannuela@gmail.com>
2022-06-08 20:06:05 +0700

    updated login page

--- dk_finances/frontend/src/pages/LoginPage.js
+++ dk_finances/frontend/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import TextField from '@mui/material/TextField'; 
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -6,36 +7,55 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/system';
 import { CssBaseline } from '@mui/material';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 
 
 function LoginPage(props){
+
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const user = { username, password };
+        fetch('http://127.0.0.1:8000/auth', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user)
+        })
+        .then((data) => data.json())
+        .then((data) => {
+            console.log(data.token);
+            }
+        )
+        .catch((error) => console.error(error))
+    }
+
     return(
-        <Container component='main' maxWidth='xs' sx={{mt: '5%'}}>
+        <Container component='main' maxWidth='xs' sx={{mt: '3%', display: "flex"}}>
             <CssBaseline>
-                <Paper elevation={12} sx={{width: 450, height: 450, pt:2}}>
-                <Typography variant="h5" align="center">
+            <Paper elevation={12} sx={{width: 700, height: 700, pt: 7}}>
+            <Typography variant="h1" align="center" sx={{ fontSize:24, fontWeight: 600 }}>
                     Login
                 </Typography>
-                <Box component='form' noValidate sx={{mt:7}}>
+                <Box component='form' noValidate sx={{ mt:15 }} onSubmit={handleLogin}>
                     <Grid>
-                        <Grid item xs={12} sm={6} sx={{pb: 2, px:4}}>
+                        <Grid item xs={12} sm={6} sx={{pb: 5, px: 12}}>
                             <TextField
-                                name="email"
+                                name="username"
                                 required
                                 fullWidth
-                                id="email"
-                                label="Email"
-                                type="email"
+                                id="username"
+                                label="Username"
+                                type="username"
                                 autoFocus
-                          
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
                             />
                         </Grid>
                     </Grid>
                     <Grid>
-                        <Grid item xs={12} sm={6} sx={{px:4}}>
+                        <Grid item xs={12} sm={6} sx={{ pb: 5, px: 12 }}>
                                 <TextField
                                 name="password"
                                 required
@@ -43,29 +63,40 @@ function LoginPage(props){
                                 id="password"
                                 label="Password"
                                 type="password"
-                                
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 />
                         </Grid>
                     </Grid>
-                    <Grid>
+                    {/* <Grid>
                         <Grid item xs={12} sx={{pt: 2, px: 4}}>
                         <FormControlLabel
                             control={<Checkbox value="allowExtraEmails" color="primary" />}
                             label="Login"
                             />
                         </Grid>
-                    </Grid>
-                    <Grid sx={{px: 6}}>
+                    </Grid> */}
+                    <Grid sx={{ px: 15}}>
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
-                            sx={{ mt: 3, mb: 2}}
+                            sx={{ mt: 10, mb: 1}}
                             >
                             Login
                         </Button>
-                    </Grid>
-                  
+                    </Grid> 
+                    <Grid sx={{ px: 15}}>
+                        <Link to="/signup" style={{ textDecoration: 'none' }}>
+                            <Button
+                                fullWidth
+                                variant="outlined"
+                                sx={{ mt: 1, mb: 2}}
+                                >
+                                Sign up
+                            </Button>
+                        </Link>
+                    </Grid>  
                 </Box>
                 </Paper>
             </CssBaseline>
